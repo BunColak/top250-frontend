4710ca18abe274c1494773185c1788fa5d843cf7
Bun Colak <bun.colak@motius.de>
2020-12-27 01:06:01 +0100

    change rating location

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -31,9 +31,9 @@ const Movie = ({ movie, refetch }: Props) => {
 
   return (
         <div className={`flex items-center justify-between ${movie.watched ? 'bg-gray-400' : 'bg-gray-200'} text-black my-2 p-2 pl-4 rounded-sm shadow cursor-pointer hover:shadow-lg active:shadow-inner`}>
-            <div className="flex items-baseline">
-                <h5 className="mr-1">{movie.title}</h5>
-                <p className="font-semibold text-sm"> - {movie.imdbRating}/10</p>
+            <div className="flex flex-col items-baseline">
+                <h5>{movie.title}</h5>
+                <p className="mt-1 font-semibold text-xs">{movie.imdbRating}/10</p>
             </div>
             <div className="cursor-pointer p-2 py-3 rounded hover:bg-red-300" onClick={handleToggleMovie}>
                 <div className={`text-xs hidden lg:block ${loading ? 'text-gray-400 select-none' : ''}`}>
