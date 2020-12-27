266b6dc494fc20da977d41cc884ea3d6e4f005d3
Bun Colak <bun.colak@motius.de>
2020-12-27 13:05:36 +0100

    styling improvements

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -8,8 +8,8 @@ import { useHistory } from 'react-router-dom'
 const MUTATION = loader('../graphql/ToggleMovie.gql')
 
 type Props = {
-    movie: IMovie
-    refetch: Function;
+  movie: IMovie
+  refetch: Function;
 }
 
 const Movie = ({ movie, refetch }: Props) => {
@@ -30,20 +30,23 @@ const Movie = ({ movie, refetch }: Props) => {
   }
 
   return (
-        <div className={`flex items-center justify-between ${movie.watched ? 'bg-gray-400' : 'bg-gray-200'} text-black my-2 p-2 pl-4 rounded-sm shadow cursor-pointer hover:shadow-lg active:shadow-inner`}>
-            <div className="flex flex-col items-baseline">
-                <h5>{movie.title}</h5>
-                <p className="mt-1 font-semibold text-xs">{movie.imdbRating}/10</p>
-            </div>
-            <div className="cursor-pointer p-2 py-3 rounded hover:bg-red-300" onClick={handleToggleMovie}>
-                <div className={`text-xs hidden lg:block ${loading ? 'text-gray-400 select-none' : ''}`}>
-                    {movie.watched ? 'Unwatch' : 'Mark As Watched'}
-                </div>
-                <div className={`flex items-center lg:hidden ml-4 ${loading ? 'text-gray-400 select-none' : ''}`}>
-                    {movie.watched ? <i className="text-base material-icons">close</i> : <i className="text-base material-icons">done</i>}
-                </div>
-            </div>
+    <div className={`flex items-center justify-between ${movie.watched ? 'bg-gray-400' : 'bg-gray-200'} text-black my-2 p-2 pl-4 rounded-sm shadow cursor-pointer hover:shadow-lg active:shadow-inner`}>
+      <div className="flex flex-col items-baseline">
+        <h5>{movie.title}</h5>
+        <div className="flex font-semibold text-xs items-baseline">
+          <p className="mt-1">{movie.imdbRating}/10</p>
+          <a className="text-indigo-800 ml-1" href={movie.link} target="_blank" rel="noopener noreferrer">IMDB<i className="material-icons text-sm leading-4 align-middle ml-1">open_in_new</i></a>
         </div>
+      </div>
+      <div className="cursor-pointer p-2 py-3 rounded lg:hover:bg-red-300" onClick={handleToggleMovie}>
+        <div className={`text-xs hidden lg:block ${loading ? 'text-gray-400 select-none' : ''}`}>
+          {movie.watched ? 'Unwatch' : 'Mark As Watched'}
+        </div>
+        <div className={`flex items-center lg:hidden ml-4 ${loading ? 'text-gray-400 select-none' : ''}`}>
+          {movie.watched ? <i className="text-base material-icons">close</i> : <i className="text-base material-icons">done</i>}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Suggestion.tsx
+++ src/components/Suggestion.tsx
@@ -8,10 +8,10 @@ import { useHistory } from 'react-router-dom'
 const MUTATION = loader('../graphql/ToggleMovie.gql')
 
 type Props = {
-    movie: IMovie | null
-    closeSuggestion: () => void;
-    newSuggestion: () => void
-    refetch: () => void;
+  movie: IMovie | null
+  closeSuggestion: () => void;
+  newSuggestion: () => void
+  refetch: () => void;
 }
 
 const Suggestion = ({ movie, newSuggestion, closeSuggestion, refetch }: Props) => {
@@ -34,18 +34,19 @@ const Suggestion = ({ movie, newSuggestion, closeSuggestion, refetch }: Props) =
   }
 
   return (
-        <div className="fixed top-0 left-0 w-full h-full text-black text-center">
-            <div className="fixed top-0 left-0 bg-black opacity-60 w-full h-full" onClick={closeSuggestion} />
-            <div className="fixed top-1/2 left-1/2 w-1/3 h-1/3 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm p-4 flex flex-col items-center justify-center" >
-                <h3 className="text-lg">Your lucky movie is....</h3>
-                <h4 className="text-4xl font-semibold my-4 mt-2">{movie?.title}</h4>
-                <a className="text-blue-800" href={movie?.link} target="_blank" rel="noopener noreferrer">Here is the IMDB link <i className="material-icons text-base leading-4 align-middle">open_in_new</i></a>
-                <div className="text-white flex">
-                    <button className="button" onClick={newSuggestion}>Gimme Another</button>
-                    <button className={`button ml-4 secondary ${loading ? 'disabled select-none' : ''}`} onClick={handleWatched}>Mark as watched</button>
-                </div>
-            </div>
+    <div className="fixed top-0 left-0 w-full h-full text-black text-center">
+      <div className="fixed top-0 left-0 bg-black opacity-60 w-full h-full" onClick={closeSuggestion} />
+      <div className="fixed top-1/2 left-1/2 w-4/5 lg:w-1/3 lg:h-1/3 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-sm p-4 pt-8 flex flex-col items-center justify-center" >
+        <button className="absolute top-2 right-2" onClick={closeSuggestion}><i className="material-icons">close</i></button>
+        <h3 className="text-lg">Your lucky movie is....</h3>
+        <h4 className="text-4xl font-semibold my-4 mt-2">{movie?.title}</h4>
+        <a className="text-blue-800" href={movie?.link} target="_blank" rel="noopener noreferrer">Here is the IMDB link <i className="material-icons text-base leading-4 align-middle">open_in_new</i></a>
+        <div className="text-white flex">
+          <button className="button" onClick={newSuggestion}>Gimme Another</button>
+          <button className={`button ml-4 secondary ${loading ? 'disabled select-none' : ''}`} onClick={handleWatched}>Mark as watched</button>
         </div>
+      </div>
+    </div>
   )
 }
 
