eed574aaf050066ddbb0220853438b7893d20768
Bün Colak <bun.colak@motius.de>
2021-09-20 17:26:27 +0200

    remove unnecessary calcc

--- src/components/Copyboard.tsx
+++ src/components/Copyboard.tsx
@@ -1,8 +1,7 @@
 import React, { useRef } from 'react'
-import { getListId } from '../services/storageService'
 
 const Copyboard = () => {
-  const LINK = `${process.env.REACT_APP_HOSTNAME}/${getListId()}`
+  const LINK = `${window.location.href}`
   const ref = useRef<HTMLTextAreaElement>(null)
 
   const onClick = () => {
@@ -13,10 +12,10 @@ const Copyboard = () => {
   }
 
   return (
-        <div className="bg-gray-300 text-black px-4 py-2 rounded-sm mt-8 w-full lg:w-auto">
+        <div className="w-full px-4 py-2 mt-8 text-black bg-gray-300 rounded-sm lg:w-auto">
             <h3>Share your list with your friends!</h3>
             <div className="flex justify-between mt-2" onClick={onClick}>
-                <textarea ref={ref} className="font-semibold text-xs border-2 border-black resize-none p-1 lg:p-2 w-full lg:w-96 h-12" value={LINK}>{LINK}</textarea>
+                <textarea ref={ref} className="w-full h-12 p-1 text-xs font-semibold border-2 border-black resize-none lg:p-2 lg:w-96" value={LINK}>{LINK}</textarea>
                 <button className="ml-2">
                     <i className="material-icons">content_copy</i>
                 </button>
